4a89bff079d3321ac31f48b1eeecb4ff2cc7247e
monghetude <rmanahan@loveyourself.ph>
2026-05-03 14:35:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -589,3 +589,28 @@ function showConfirmModal(onConfirm) {
   document.getElementById("cancelBtn").style.opacity = "100%";
   };
 }
+
+function checkSession() {
+  const token = localStorage.getItem("sessionToken");
+
+  if (!token) {
+    window.location.href = "index.html";
+  }
+
+  fetch("https://script.google.com/macros/s/AKfycbwYhaIIxax9_IjEqW6KlK8p7l2eMiB7zDhEJwI350SeEl-3oxt4T1WNnHn0VyUgmlFz/exec", {
+    method: "POST",
+    body: JSON.stringify({
+      action: "validateSession",
+      sessionToken: token
+    })
+  })
+  .then(r => r.json())
+  .then(res => {
+    if (!res.valid) {
+      localStorage.clear();
+      window.location.href = "index.html";
+    }
+  });
+}
+
+setInterval(checkSession, 300000); // every 5 min
