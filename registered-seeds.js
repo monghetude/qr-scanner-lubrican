009176a7db82813528829774ec8ae51fa30ef6e1
protects <rmanahan@loveyourself.ph>
2026-05-03 20:05:19 +0800

    Update registered-seeds.js

--- registered-seeds.js
+++ registered-seeds.js
@@ -71,7 +71,7 @@ fetch("https://script.google.com/macros/s/AKfycbwYhaIIxax9_IjEqW6KlK8p7l2eMiB7zD
     action: "getSeeds",
     sessionToken: localStorage.getItem("sessionToken")
   })
-  console.log(sessionToken);
+  console.log(localStorage.getItem("sessionToken"));
 })
 .then(res => res.json())
 .then(data => {
