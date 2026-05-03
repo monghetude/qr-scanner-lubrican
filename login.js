5b3b6045fd47090148b9d93bbbf4a65d45778164
monghetude <rmanahan@loveyourself.ph>
2026-05-03 12:49:57 +0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,20 @@
+document.getElementById("loginBtn").addEventListener("click", loginUser);
+
+function loginUser() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // TEMPORARY hardcoded validation
+  // Later this should come from GAS backend
+
+  if (username === "admin" && password === "1234") {
+    localStorage.setItem("loggedIn", "true");
+    localStorage.setItem("username", username);
+    localStorage.setItem("cbo", "LoveYourself");
+    localStorage.setItem("role", "admin");
+
+    window.location.href = "scanner.html";
+  } else {
+    document.getElementById("loginMessage").innerText = "Invalid login";
+  }
+}
