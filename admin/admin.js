91609052a5ba3665b1f51f4e46965d0f215b9f41
monghetude <rmanahan@loveyourself.ph>
2026-05-03 14:54:45 +0800

    Create admin.js

---
+++ admin/admin.js
@@ -0,0 +1,21 @@
+function createUser() {
+  fetch("https://script.google.com/macros/s/AKfycbwYhaIIxax9_IjEqW6KlK8p7l2eMiB7zDhEJwI350SeEl-3oxt4T1WNnHn0VyUgmlFz/exec", {
+    method: "POST",
+    body: JSON.stringify({
+      action: "createUser",
+      sessionToken: localStorage.getItem("sessionToken"),
+      username: document.getElementById("newUsername").value,
+      password: document.getElementById("newPassword").value,
+      role: document.getElementById("newRole").value,
+      cbo: document.getElementById("newCbo").value
+    })
+  })
+  .then(r => r.json())
+  .then(res => {
+    if (res.success) {
+      alert("User created");
+    } else {
+      alert(res.message);
+    }
+  });
+}
